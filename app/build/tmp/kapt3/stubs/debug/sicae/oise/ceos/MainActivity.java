6a8e4801c0edd68aa53ce1ccc69275ed4c02beb0
Julien BARBE <43819521+wakipa@users.noreply.github.com>
2022-03-17 12:22:19 +0100

    Add files via upload

---
+++ app/build/tmp/kapt3/stubs/debug/sicae/oise/ceos/MainActivity.java
@@ -0,0 +1,18 @@
+package sicae.oise.ceos;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 4, 0}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u0018\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0010\u0010\u0003\u001a\u00020\u00042\u0006\u0010\u0005\u001a\u00020\u0006H\u0016\u00a8\u0006\u0007"}, d2 = {"Lsicae/oise/ceos/MainActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "btnFormulaire", "", "view", "Landroid/view/View;", "app_debug"})
+public final class MainActivity extends androidx.appcompat.app.AppCompatActivity {
+    
+    /**
+     * Appelée lorsque l'utilisateur tape le bouton "Accéder au formulaire"
+     */
+    public void btnFormulaire(@org.jetbrains.annotations.NotNull()
+    android.view.View view) {
+    }
+    
+    public MainActivity() {
+        super();
+    }
+}
